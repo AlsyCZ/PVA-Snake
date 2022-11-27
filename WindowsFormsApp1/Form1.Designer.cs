59996f597b23b28177f98bf1e578d26006dff821
Aleš Mrázek <100871677+AlsyCZ@users.noreply.github.com>
2022-11-27 12:00:00 +0100

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel3.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.lol;
+            this.panel3.Location = new System.Drawing.Point(558, 335);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(51, 62);
+            this.panel3.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(558, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(558, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1177, 671);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel3);
+            this.Name = "Form1";
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_PreviewKeyDown_1(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.D && this.Width > panel3.Location.X)
+            {
+                panel3.Location = new Point(panel3.Location.X + 10, panel3.Location.Y);
+                label1.Text = "X: " + panel3.Location.X;
+                label2.Text = "Y: " + panel3.Location.Y;
+            } else if(e.KeyCode == Keys.D && panel3.Location.X >= this.Width)
+            {
+                panel3.Location = new Point(0, panel3.Location.Y);
+            }
+            if (e.KeyCode == Keys.A && panel3.Location.X > 0)
+            {
+                panel3.Location = new Point(panel3.Location.X - 10, panel3.Location.Y);
+                label1.Text = "X: " + panel3.Location.X;
+                label2.Text = "Y: " + panel3.Location.Y;
+            }
+            else if (e.KeyCode == Keys.A && panel3.Location.X <= 0)
+            {
+                panel3.Location = new Point(this.Width, panel3.Location.Y);
+            }
+            if (e.KeyCode == Keys.W && panel3.Location.Y > 0)
+            {
+                label1.Text = "X: " + panel3.Location.X;
+                label2.Text = "Y: " + panel3.Location.Y;
+                panel3.Location = new Point(panel3.Location.X, panel3.Location.Y - 10);
+            }
+            else if (e.KeyCode == Keys.W && panel3.Location.Y <= 0)
+            {
+                panel3.Location = new Point(panel3.Location.X, this.Height);
+            }
+            if (e.KeyCode == Keys.S && panel3.Location.Y < this.Height)
+            {
+                label1.Text = "X: " + panel3.Location.X;
+                label2.Text = "Y: " + panel3.Location.Y;
+                panel3.Location = new Point(panel3.Location.X, panel3.Location.Y + 10);
+            }
+            else if (e.KeyCode == Keys.S && panel3.Location.Y >= this.Height)
+            {
+                panel3.Location = new Point(panel3.Location.X,0);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
